1456eaf9b13dae08ef2452930716609b7ac5e0b2
Jay Mohile <jamohile@google.com>
2020-06-29 17:57:02 -0400

    create initial boilerplate

---
+++ app/src/popup/Popup.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+class Popup extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello World!</h1>
+      </div>
+    );
+  }
+}
+
+// Mounts popup once html is loaded
+document.addEventListener(
+  "DOMContentLoaded",
+  () => {
+    const appMount = document.querySelector("#app");
+    ReactDOM.render(<Popup />, appMount);
+  },
+  false
+);
