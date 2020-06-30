61427982d9ef7db37d003497d1c2bce001a42c71
Jay Mohile <jamohile@google.com>
2020-06-30 11:56:36 -0400

    feat: add background request class

--- app/src/common/requests/BackgroundRequest.ts
+++ app/src/common/requests/BackgroundRequest.ts
@@ -15,11 +15,25 @@ export enum BackgroundRequestType {
  * A standardized format for all background requests.
  * Each type of request must extend this with its required parameters.
  */
-export abstract class BackgroundRequestData{
+export abstract class BackgroundRequestData {
     /** Used for dispatching to correct handler. */
     type: BackgroundRequestType;
 
     constructor(type: BackgroundRequestType) {
         this.type = type;
+    }
+}
+
+/**
+ * Used to send requests (with optional response) from UI to API background.
+ * This isn't meant to be used directly, instead through derived classes for each request type.
+ */
+export abstract class BackgroundRequest<D extends BackgroundRequestData, R> {
+    run(data: D): Promise<R> {
+        return new Promise((resolve) => {
+            chrome.runtime.sendMessage(data, (response: R) => {
+                resolve(response);
+            });
+        });
     }
 }
