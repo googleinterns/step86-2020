8a57c2d5e3a5ad9792e8d2c82c7f7586a3eaa3f6
KushalD1999 <kdalal@google.com>
2020-06-30 17:31:58 -0400

    Implements fetchProject and exports modules

--- api/src/core-api.js
+++ api/src/core-api.js
@@ -1,6 +1,25 @@
 
-var userAuth = NULL;
+var userAuth = null;
 
-export function setAuthToken(token){
+function setAuthToken(token){
   userAuth = token; 
 }
+
+async function fetchProjects() {
+  // if (userAuth !== NULL) {
+    const response = await fetch("https://cloudresourcemanager.googleapis.com/v1/projects", {
+        method: "GET",
+        headers: {
+            Authorization: `Bearer ${userAuth}`
+        }
+    });
+    const json = await response.json();
+  // }
+  return json;
+}
+
+console.log(fetchProjects); 
+
+
+module.exports = setAuthToken;
+module.exports = fetchProjects;
