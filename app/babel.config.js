afd9793cbd3046ec657ea272dc8894c98ca52cd8
Jay Mohile <jamohile@google.com>
2020-06-30 22:02:14 -0400

    fix: run prettier on config files

--- app/babel.config.js
+++ app/babel.config.js
@@ -1,13 +1,16 @@
 module.exports = {
-    presets: [
-        "@babel/preset-env",
-        "@babel/preset-react",
-        "@babel/preset-typescript"
+  presets: [
+    "@babel/preset-env",
+    "@babel/preset-react",
+    "@babel/preset-typescript",
+  ],
+  plugins: [
+    [
+      "@babel/plugin-transform-runtime",
+      {
+        helpers: false,
+        regenerator: true,
+      },
     ],
-    plugins: [
-        ["@babel/plugin-transform-runtime", {
-            "helpers": false,
-            "regenerator": true
-        }]
-    ]
-}
+  ],
+};
