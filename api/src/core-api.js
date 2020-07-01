385bfe7141c8dbdfabf20fb1212a157bd8aec420
KushalD1999 <kdalal@google.com>
2020-07-01 00:03:20 -0400

    Implements fetchDebuggees api that returs all list of active debugees

--- api/src/core-api.js
+++ api/src/core-api.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 
 
-var userAuth = "ya29.a0AfH6SMDabecJYQOPnoOaCoD6L8wGwt9lLtP7ZJMpH7-gZdTOcdc28HdsDpvRberPDik_n2qxFQira648ingk-7VRhi86S54so0VMDxqhQx04jSypfQDyeE9a-DBDqJ3axAOUBp0oq8GprL_lpJk2inPNByWdk5PW5cQ"; // Temp token
+var userAuth = "ya29.a0AfH6SMCCEwdJKvs-NN2c34UB5PEVD-61tTmphALVUoEKKZKLgRifzgluUXwBN1TJbzKBROpsyuR8Qq6usq6-AmGGPMmbbq5BXhboXK6kOQZJ3OSFmJ_pdN0bdoR2Fo2u6nc3yMiMe_t9rEf55PV5QWalYja2oIko8lY"; // Temp token
 
 function setAuthToken(token){
   userAuth = token; 
@@ -17,10 +17,26 @@ async function fetchProjects() {
     return data;
 }
 
-async function  main()  {
-  console.dir(await fetchProjects())  
+
+async function fetchDebuggees(projectId) {
+  const response = await axios.get("https://clouddebugger.googleapis.com/v2/debugger/debuggees",{
+    headers: {
+      Authorization: `Bearer ${userAuth}`
+    },
+    params: {
+      project: projectId,
+    }
+  });
+  let data = response.data;
+  return data;
 }
 
+
+
+async function  main()  {
+    console.dir(await fetchDebuggees("kdalal-step-2020"))
+  }
+  
 main();
 
 module.exports = setAuthToken;
