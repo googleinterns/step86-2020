21a9fd6c5224f3439b8dfc74fd7e952f9ddc7b6c
KushalD1999 <kdalal@google.com>
2020-06-30 21:53:18 -0400

    Implements fetchProject using axios

--- api/src/core-api.js
+++ api/src/core-api.js
@@ -1,25 +1,27 @@
+const axios = require('axios');
 
-var userAuth = null;
+
+var userAuth = "ya29.a0AfH6SMDabecJYQOPnoOaCoD6L8wGwt9lLtP7ZJMpH7-gZdTOcdc28HdsDpvRberPDik_n2qxFQira648ingk-7VRhi86S54so0VMDxqhQx04jSypfQDyeE9a-DBDqJ3axAOUBp0oq8GprL_lpJk2inPNByWdk5PW5cQ"; // Temp token
 
 function setAuthToken(token){
   userAuth = token; 
 }
 
 async function fetchProjects() {
-  // if (userAuth !== NULL) {
-    const response = await fetch("https://cloudresourcemanager.googleapis.com/v1/projects", {
-        method: "GET",
-        headers: {
-            Authorization: `Bearer ${userAuth}`
-        }
+    const response = await axios.get("https://cloudresourcemanager.googleapis.com/v1/projects",{
+      headers: {
+        Authorization: `Bearer ${userAuth}`
+      }
     });
-    const json = await response.json();
-  // }
-  return json;
+    let data = response.data;
+    return data;
 }
 
-console.log(fetchProjects); 
+async function  main()  {
+  console.dir(await fetchProjects())  
+}
 
+main();
 
 module.exports = setAuthToken;
 module.exports = fetchProjects;
