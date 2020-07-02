2440553ed4b5df7af059119003d61208b522f71e
hanane ikhelef <hananeikhelef@google.com>
2020-07-02 19:10:34 -0400

    feat: add file and line input

--- app/src/client/injected/injected-app.tsx
+++ app/src/client/injected/injected-app.tsx
@@ -4,7 +4,6 @@ import styled from 'styled-components';
 import {showAlert} from "../injected/temporary";
 showAlert();
 
-
 class InjectedApp extends React.Component {
 
   constructor(){
@@ -13,23 +12,55 @@ class InjectedApp extends React.Component {
 
   render() {
     return (
-      <div>
-        <h1> Welcome to chathead !</h1>
-        <button onClick={showAlert}>Show an Alert</button>
-      </div>
+      <Wrapper>
+        <p>Create a Break Point</p>
+        <Input placeholder="File Name " />
+        <Input placeholder="Line Number " />
+        <Button primary onClick={showAlert}>CREATE</Button>
+      </Wrapper>
     );
   }
 
 }
 
-// Mounts popup once html is loaded
-//document.addEventListener(
- // "DOMContentLoaded",
-  //() => {
-    // const appMount = document.querySelector("#injected-app");
-    const mount = document.createElement("div");
-    document.body.appendChild(mount);
-    ReactDOM.render(<InjectedApp />, mount);
-//  },
-  //false
-//);
+// Mounts injected App once html is loaded
+
+const mount = document.createElement("div");
+document.body.appendChild(mount);
+ReactDOM.render(<InjectedApp />, mount);
+
+const Wrapper = styled.section`
+  padding: 6em;
+  float:right; 
+  background:white; 
+  width:25%; 
+  height:450px; 
+  position:fixed;
+  top:50px;
+  right:20px; 
+  box-shadow: 0 0 6px #000;
+  z-index: 10000; 
+`;
+
+const Input = styled.input.attrs(props => ({
+  size: props.size || "1em",
+}))`
+ color: #1E90FF;
+  font-size: 1em;
+  border: 2px solid #1E90FF;
+  border-radius: 3px;
+  margin: ${props => props.size};
+  padding: ${props => props.size};
+`;
+
+const Button = styled.button`
+  /* Adapt the colors based on primary prop */
+  background: ${props => props.primary ? "#1E90FF" : "white"};
+  color: ${props => props.primary ? "white" : "#1E90FF"};
+
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid #1E90FF;
+  border-radius: 3px;
+`;
--- app/src/client/injected/temporary.ts
+++ app/src/client/injected/temporary.ts
@@ -1,3 +1,21 @@
+
 export function showAlert() {
     alert("This is an alert!");
+}
+
+// can be used for milstone 2
+export function addBreakpoint(){
+
+    document.querySelectorAll(".js-line-number").forEach(node => {
+     document.getElementById(node.getAttribute("id")).onclick = changeColor;
+    });
+}
+
+export function changeColor(){
+
+  document.querySelectorAll(".js-line-number").forEach(node => {
+  document.getElementById(node.getAttribute("id")).style.color = "blue";
+  document.getElementById(node.getAttribute("id")).style.backgroundColor = " #77a1ee";
+  });
+
 }
