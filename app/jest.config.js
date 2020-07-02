a03ac7af70190f8739b5cd9df6378d5752530422
Jay Mohile <jamohile@google.com>
2020-07-02 13:08:47 -0400

    fix: run prettier

--- app/jest.config.js
+++ app/jest.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    preset: "jest-puppeteer" // Lets us integration test the extension by loading it into a controllable browser window.
-}
+  preset: "jest-puppeteer", // Lets us integration test the extension by loading it into a controllable browser window.
+};
