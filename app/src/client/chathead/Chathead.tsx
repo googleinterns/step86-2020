bbf66b1d63ffcf02c444dbfd6311a655a18398f8
Jay Mohile <jamohile@google.com>
2020-07-03 15:36:58 -0400

    feat: add scaffold for chathead

--- app/src/client/chathead/Chathead.tsx
+++ app/src/client/chathead/Chathead.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import styled from "styled-components";
 
 export const Test = () => <div>Test</div>;
 
@@ -15,6 +16,39 @@ interface ChatheadProps {
 interface ChatheadState {
     /** All projects this user has access to, loaded using FetchProjectsRequest */
     projects: any[]; // TODO: Create a Project type
+    projectsLoading: boolean;
+
     /** All debuggees for the current project, loaded using FetchDebuggeesRequest */
-    debuggees: any[]; // TODO: Create a Debuggee type
+    debuggees: any[]; // TODO: Create a Debuggee type,
+    debuggeesLoading: boolean;
 }
+
+export class Chathead extends React.Component<ChatheadProps, ChatheadState> {
+    constructor(props: ChatheadProps) {
+        super(props);
+        this.state = {
+            projects: [],
+            projectsLoading: false,
+
+            debuggees: [],
+            debuggeesLoading: false
+        }
+    }
+
+    render() {
+        return (
+            <ChatheadWrapper>
+            </ChatheadWrapper>
+        )
+    }
+}
+
+const ChatheadWrapper = styled.div`
+    position: fixed;
+    top: 20px;
+    right: 20px;
+
+    padding: 20px;
+
+    box-shadow: 0px 2px 8px 0px rgba(20, 20, 20, 0.4);
+`;
