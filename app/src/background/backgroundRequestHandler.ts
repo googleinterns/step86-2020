812532242448a9e084e9e42cd72c99480e507da3
KushalD1999 <kdalal@google.com>
2020-07-06 11:04:32 -0400

    feat: Create background handler for fetch projects

---
+++ app/src/background/backgroundRequestHandler.ts
@@ -0,0 +1,29 @@
+import api from "debugger-extension-api";
+
+
+
+
+BackgroundRequestHandler.on(BackgroundRequestType.FETCH_PROJECT, async data => {
+    const response = await api.fetchProjects(); 
+    return response;
+})
+
+BackgroundRequestHandler.on(BackgroundRequestType.FETCH_DEBUGGEES, async data => {
+    const response = await api.fetchDebuggees(data.projectId); 
+    return response;
+})
+
+BackgroundRequestHandler.on(BackgroundRequestType.SET_BREAKPOINT, async data => {
+    const response = await api.setBreakpoint(data.debuggeeId, data.fileName, data.lineNumber); 
+    return response;
+})
+
+BackgroundRequestHandler.on(BackgroundRequestType.FETCH_BREAKPOINT, async data => {
+    const response = await api.getBreakpoint(data.debuggeeId, data.breakpointId); 
+    return response;
+})
+
+BackgroundRequestHandler.on(BackgroundRequestType.LIST_BREAKPOINTS, async data => {
+    const response = await api.fetchDebuggees(data.debuggeeId); 
+    return response;
+}
