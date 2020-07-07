63226dd07fd58bac35869a7e6e456f09aecb45d7
KushalD1999 <kdalal@google.com>
2020-07-06 23:31:57 -0400

    Update documentation

--- app/src/background/backgroundRequestHandler.ts
+++ app/src/background/backgroundRequestHandler.ts
@@ -1,16 +1,26 @@
 import api from "debugger-extension-api";
 import * as backgroundRequest from "../common/requests/BackgroundRequest";
 
+/**
+ * BackgroundRequestHandler receives chrome runtime messages (i.e. BackgroundRequestData) and it
+ * dispatches them to the handlers based on their type (BackgroundRequestType) field. 
+ * These handlers will perform  logic usually by calling (and awaiting) debugger-extension API methods.
+ */
 class BackgroundRequestHandler {
+  // Dictionary to keep the handlers and their callback
   static handlers = {};
 
   static on<D extends backgroundRequest.BackgroundRequestData>(
     type: backgroundRequest.BackgroundRequestType,
     callback: (data: D) => Promise<any>
   ) {
-    backgroundRequest.BackgroundRequestHandler.handlers[type] = callback;
+    BackgroundRequestHandler.handlers[type] = callback;
   }
 
+  /**
+ * Static functionto match the handler based on their BackgroundRequest type and send the response
+ * back to the request using chrome.runtime.onMessage.addListener(function callback)
+ */
   static listen() {
     chrome.runtime.onMessage.addListener(
       async (data: backgroundRequest.BackgroundRequestData, sender, sendResponse) => {
@@ -25,6 +35,9 @@ class BackgroundRequestHandler {
   }
 }
 
+/**
+ * Handler for fetching the project from debugger-extension api and return the response.
+ */
 BackgroundRequestHandler.on<backgroundRequest.FetchProjectsRequestData>(
   backgroundRequest.BackgroundRequestType.FETCH_PROJECTS,
   async (data) => {
@@ -33,6 +46,9 @@ BackgroundRequestHandler.on<backgroundRequest.FetchProjectsRequestData>(
   }
 );
 
+/**
+ * Handler for fetching the debuggees from debugger-extension api and return the response.
+ */
 BackgroundRequestHandler.on<backgroundRequest.FetchDebuggeesRequestData>(
   backgroundRequest.BackgroundRequestType.FETCH_DEBUGGEES,
   async (data) => {
@@ -41,6 +57,9 @@ BackgroundRequestHandler.on<backgroundRequest.FetchDebuggeesRequestData>(
   }
 );
 
+/**
+ * Handler for setting the breakpoints using debugger-extension api and return the response.
+ */
 BackgroundRequestHandler.on<backgroundRequest.SetBreakpointRequestData>(
   backgroundRequest.BackgroundRequestType.SET_BREAKPOINT,
   async (data) => {
@@ -53,6 +72,9 @@ BackgroundRequestHandler.on<backgroundRequest.SetBreakpointRequestData>(
   }
 );
 
+/**
+ * Handler for fetching the breakpoints from debugger-extension api and return the response.
+ */
 BackgroundRequestHandler.on<backgroundRequest.FetchBreakpointRequestData>(
   backgroundRequest.BackgroundRequestType.FETCH_BREAKPOINT,
   async (data) => {
@@ -64,6 +86,9 @@ BackgroundRequestHandler.on<backgroundRequest.FetchBreakpointRequestData>(
   }
 );
 
+/**
+ * Handler for listing the breakpoints from debugger-extension api and return the response.
+ */
 BackgroundRequestHandler.on<backgroundRequest.ListBreakpointsData>(
   backgroundRequest.BackgroundRequestType.LIST_BREAKPOINTS,
   async (data) => {
