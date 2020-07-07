2b2a8c7fffec894cd1b0d3fb91601ae4bbc470df
KushalD1999 <kdalal@google.com>
2020-07-06 22:20:42 -0400

    fix: import backgroundRequest class

--- app/src/background/backgroundRequestHandler.ts
+++ app/src/background/backgroundRequestHandler.ts
@@ -1,22 +1,19 @@
 import api from "debugger-extension-api";
-import {
-  BackgroundRequestType,
-  BackgroundRequestData,
-} from "../common/requests/BackgroundRequest";
+import * as backgroundRequest from "../common/requests/BackgroundRequest";
 
 class BackgroundRequestHandler {
   static handlers = {};
 
-  static on<D extends BackgroundRequestData>(
-    type: BackgroundRequestType,
+  static on<D extends backgroundRequest.BackgroundRequestData>(
+    type: backgroundRequest.BackgroundRequestType,
     callback: (data: D) => Promise<any>
   ) {
-    BackgroundRequestHandler.handlers[type] = callback;
+    backgroundRequest.BackgroundRequestHandler.handlers[type] = callback;
   }
 
   static listen() {
     chrome.runtime.onMessage.addListener(
-      async (data: BackgroundRequestData, sender, sendResponse) => {
+      async (data: backgroundRequest.BackgroundRequestData, sender, sendResponse) => {
         const handler = BackgroundRequestHandler.handlers[data.type];
         if (handler === undefined) {
           throw new Error("Handler not registered for type: " + data.type);
@@ -28,24 +25,24 @@ class BackgroundRequestHandler {
   }
 }
 
-BackgroundRequestHandler.on<FetchProjectsRequestData>(
-  BackgroundRequestType.FETCH_PROJECT,
+BackgroundRequestHandler.on<backgroundRequest.FetchProjectsRequestData>(
+  backgroundRequest.BackgroundRequestType.FETCH_PROJECTS,
   async (data) => {
     const response = await api.fetchProjects();
     return response;
   }
 );
 
-BackgroundRequestHandler.on<FetchDebuggeesRequestData>(
-  BackgroundRequestType.FETCH_DEBUGGEES,
+BackgroundRequestHandler.on<backgroundRequest.FetchDebuggeesRequestData>(
+  backgroundRequest.BackgroundRequestType.FETCH_DEBUGGEES,
   async (data) => {
     const response = await api.fetchDebuggees(data.projectId);
     return response;
   }
 );
 
-BackgroundRequestHandler.on<SetBreakpointRequestData>(
-  BackgroundRequestType.SET_BREAKPOINT,
+BackgroundRequestHandler.on<backgroundRequest.SetBreakpointRequestData>(
+  backgroundRequest.BackgroundRequestType.SET_BREAKPOINT,
   async (data) => {
     const response = await api.setBreakpoint(
       data.debuggeeId,
@@ -56,8 +53,8 @@ BackgroundRequestHandler.on<SetBreakpointRequestData>(
   }
 );
 
-BackgroundRequestHandler.on<FetchBreakpointRequestData>(
-  BackgroundRequestType.FETCH_BREAKPOINT,
+BackgroundRequestHandler.on<backgroundRequest.FetchBreakpointRequestData>(
+  backgroundRequest.BackgroundRequestType.FETCH_BREAKPOINT,
   async (data) => {
     const response = await api.getBreakpoint(
       data.debuggeeId,
@@ -67,8 +64,8 @@ BackgroundRequestHandler.on<FetchBreakpointRequestData>(
   }
 );
 
-BackgroundRequestHandler.on<ListBreakpointsData>(
-  BackgroundRequestType.LIST_BREAKPOINTS,
+BackgroundRequestHandler.on<backgroundRequest.ListBreakpointsData>(
+  backgroundRequest.BackgroundRequestType.LIST_BREAKPOINTS,
   async (data) => {
     const response = await api.fetchDebuggees(data.debuggeeId);
     return response;
