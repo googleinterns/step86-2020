42311ae61a59987315cd3f791902b3567f31f857
KushalD1999 <kdalal@google.com>
2020-07-10 14:08:09 -0400

    Fix: Remove getToken from background script

--- app/src/background/background.ts
+++ app/src/background/background.ts
@@ -2,10 +2,5 @@ import * as extensionAuthHandler from "./extensionAuthHandler";
 import * as backgroundRequestHandler from "./backgroundRequestHandler";
 
 window.onload = (event) => {
-  // Refreshes Auth token every 5 minutes
-  setInterval(() => {
-    extensionAuthHandler.getToken();
-  }, 5 * 60 * 1000); 
-  extensionAuthHandler.getToken(); // Also authenticates immediately.
   backgroundRequestHandler.BackgroundRequestHandler.listen();
 };
