ae5096d4b2bc0ae3aa5b416c4e26b42a94b0f069
KushalD1999 <kdalal@google.com>
2020-07-10 11:21:19 -0400

    fix: refreshes OAuth token in api at an interval of 5 minutes

--- app/src/background/background.ts
+++ app/src/background/background.ts
@@ -2,6 +2,9 @@ import * as extensionAuthHandler from "./extensionAuthHandler";
 import * as backgroundRequestHandler from "./backgroundRequestHandler";
 
 window.onload = (event) => {
-  extensionAuthHandler.getToken();
+  // Refreshes Auth token every 5 minutes
+  setInterval( () => {
+    extensionAuthHandler.getToken();
+  }, 5 * 60 * 1000); 
   backgroundRequestHandler.BackgroundRequestHandler.listen();
 };
