7ead069c027bfd41c66a3cd96a0269e60e77643b
Jay Mohile <jamohile@google.com>
2020-07-10 12:45:35 -0400

    fix: run prettier on breakpoint marker code

--- app/src/client/markers/BreakpointMarker.tsx
+++ app/src/client/markers/BreakpointMarker.tsx
@@ -1,59 +1,63 @@
 import React from "react";
-import styled, {keyframes} from "styled-components";
+import styled, { keyframes } from "styled-components";
 
 import { BreakpointMeta, Breakpoint } from "../../common/types/debugger";
 
 interface NewBreakpointMarkerProps {
-    onClick: () => void;
+  onClick: () => void;
 }
 
 /** Displayed next to line number to set new breakpoint when clicked. */
-export const NewBreakpointMarker = ({onClick}: NewBreakpointMarkerProps) => {
-    return <NewBreakpointMarkerWrapper onClick={onClick}/>
-}
+export const NewBreakpointMarker = ({ onClick }: NewBreakpointMarkerProps) => {
+  return <NewBreakpointMarkerWrapper onClick={onClick} />;
+};
 
 interface ActiveBreakpointMarkerProps {
-    breakpointMeta: BreakpointMeta;
+  breakpointMeta: BreakpointMeta;
 }
 
 /** Displayed next to line number to show currently active (pending) breakpoints */
-export const ActiveBreakpointMarker = ({breakpointMeta}: ActiveBreakpointMarkerProps) => {
-    return <ActiveBreakpointMarkerWrapper/>
-}
+export const ActiveBreakpointMarker = ({
+  breakpointMeta,
+}: ActiveBreakpointMarkerProps) => {
+  return <ActiveBreakpointMarkerWrapper />;
+};
 
 interface CompletedBreakpointMarkerProps {
-    breakpoint: Breakpoint;
+  breakpoint: Breakpoint;
 }
 
 /** Displayed next to line number to show completed breakpoints */
-export const CompletedBreakpointMarker = ({breakpoint}: CompletedBreakpointMarkerProps) => {
-    return <CompletedBreakpointMarkerWrapper/>
-}
+export const CompletedBreakpointMarker = ({
+  breakpoint,
+}: CompletedBreakpointMarkerProps) => {
+  return <CompletedBreakpointMarkerWrapper />;
+};
 
 /** UI that is shared by all types of breakpoint markers.
  *  Defines basic shape and positioning.
  */
 const GeneralBreakpointMarkerWrapper = styled.div`
-    width: 16px;
-    height: 20px;
-    margin-left: 4px;
+  width: 16px;
+  height: 20px;
+  margin-left: 4px;
 
-    background-clip: content-box;
-    border: 4px solid white;
-    border-left: none;
-    border-right: none;
-    clip-path: polygon(0% 0%, 50% 0%, 100% 50%, 50% 100%, 0% 100%);
-    box-shadow: inset 0px -6px 0px rgba(0, 0, 0, 0.1);
+  background-clip: content-box;
+  border: 4px solid white;
+  border-left: none;
+  border-right: none;
+  clip-path: polygon(0% 0%, 50% 0%, 100% 50%, 50% 100%, 0% 100%);
+  box-shadow: inset 0px -6px 0px rgba(0, 0, 0, 0.1);
 `;
 
 /** Makes new breakpoint button invisible until hovered. */
 const NewBreakpointMarkerWrapper = styled(GeneralBreakpointMarkerWrapper)`
+  background: #ccc;
+  opacity: 0;
+  &:hover {
     background: #ccc;
-    opacity: 0;
-    &:hover {
-        background: #ccc;
-        opacity: 1;
-    }
+    opacity: 1;
+  }
 `;
 
 /** Animation to show loading state of active breakpoints */
@@ -64,15 +68,15 @@ const ActiveBreakpointMarkerPulsate = keyframes`
   to {
       transform: scale(1.1);
   }
-`
+`;
 
 /** Makes active breakpoints yellow (like a traffic light...halfway), and pulse. */
 const ActiveBreakpointMarkerWrapper = styled(GeneralBreakpointMarkerWrapper)`
-    background: gold;
-    animation: ${ActiveBreakpointMarkerPulsate} 0.4s alternate infinite;
+  background: gold;
+  animation: ${ActiveBreakpointMarkerPulsate} 0.4s alternate infinite;
 `;
 
 /** Makes completed breakpoints appear blue and static. */
 const CompletedBreakpointMarkerWrapper = styled(GeneralBreakpointMarkerWrapper)`
-    background: cornflowerblue;
+  background: cornflowerblue;
 `;
--- app/src/client/markers/BreakpointMarkers.tsx
+++ app/src/client/markers/BreakpointMarkers.tsx
@@ -2,16 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import { BreakpointMeta, Breakpoint } from "../../common/types/debugger";
-import { ActiveBreakpointMarker, CompletedBreakpointMarker, NewBreakpointMarker } from "./BreakpointMarker";
+import {
+  ActiveBreakpointMarker,
+  CompletedBreakpointMarker,
+  NewBreakpointMarker,
+} from "./BreakpointMarker";
 
 interface BreakpointMarkersProps {
-    /** All breakpoints that have been set by the current user, and are not yet completed */
-    activeBreakpoints: BreakpointMeta[];
-    /** All breakpoints that have been set by tht current user, and completed. */
-    completedBreakpoints: Breakpoint[];
+  /** All breakpoints that have been set by the current user, and are not yet completed */
+  activeBreakpoints: BreakpointMeta[];
+  /** All breakpoints that have been set by tht current user, and completed. */
+  completedBreakpoints: Breakpoint[];
 
-    /** Function to set a new breakpoint */
-    createBreakpoint: (fileName: string, lineNumber: number) => void;
+  /** Function to set a new breakpoint */
+  createBreakpoint: (fileName: string, lineNumber: number) => void;
 }
 
 /** Injects breakpoint markers for all source-code-lines on current page.
@@ -21,83 +25,107 @@ interface BreakpointMarkersProps {
  *   - Completed breakpoint on line => marker to indicate completed status.
  */
 export class BreakpointMarkers extends React.Component<BreakpointMarkersProps> {
-    /** Each line number has a DOM node to mount its marker to.
-     *  These are kept in memory so we don't have to constantly make (expensive) querySelector operations.
-     */
-    mountNodes =  new Map<number, Node>();
+  /** Each line number has a DOM node to mount its marker to.
+   *  These are kept in memory so we don't have to constantly make (expensive) querySelector operations.
+   */
+  mountNodes = new Map<number, Node>();
 
-    /** Infers the current source file name based on github UI elements.
-     *  TODO: make this compatible with other sites.
-     */
-    getFileName(): string {
-        return document.querySelector(".final-path").innerHTML;
+  /** Infers the current source file name based on github UI elements.
+   *  TODO: make this compatible with other sites.
+   */
+  getFileName(): string {
+    return document.querySelector(".final-path").innerHTML;
+  }
+
+  /** Get list of DOM nodes for each row of source code in the currently active file.
+   *  These are used to mount breakpoint markers.
+   *  TODO: make this compatible with non-github too.
+   */
+  getRowNodes(): Node[] {
+    // Notice the map to node.parentNode. This lets us target the <tr/> container rather than the <td/> line number.
+    // This is because (on github at least) clicking the <td/> directly causes a navigation event.
+    // Mounting on the <tr/> and positioning properly is a workaround.
+    //@ts-ignore
+    return [...document.querySelectorAll(".js-line-number")].map(
+      (node) => node.parentNode
+    );
+  }
+
+  /** While we recieve an array of active/completed breakpoints, we need to know whether such a breakpoint
+   * exists for a given line number. (Else, we show a 'new' button).
+   * This indexes breakpoints by line number.
+   */
+  indexByLineNumber<T extends BreakpointMeta>(
+    breakpoints: T[]
+  ): Map<number, T> {
+    const indexedBreakpoints = new Map<number, T>();
+    for (const bp of breakpoints) {
+      indexedBreakpoints.set(bp.location.line, bp);
     }
+    return indexedBreakpoints;
+  }
 
-    /** Get list of DOM nodes for each row of source code in the currently active file.
-     *  These are used to mount breakpoint markers.
-     *  TODO: make this compatible with non-github too.
-     */
-    getRowNodes(): Node[] {
-      // Notice the map to node.parentNode. This lets us target the <tr/> container rather than the <td/> line number.
-      // This is because (on github at least) clicking the <td/> directly causes a navigation event.
-      // Mounting on the <tr/> and positioning properly is a workaround.
-      //@ts-ignore
-      return [...document.querySelectorAll(".js-line-number")].map(node => node.parentNode);
-    }
+  render() {
+    const rowNodes = this.getRowNodes();
+    const activeBreakpoints = this.indexByLineNumber(
+      this.props.activeBreakpoints
+    );
+    const completedBreakpoints = this.indexByLineNumber(
+      this.props.completedBreakpoints
+    );
 
-    /** While we recieve an array of active/completed breakpoints, we need to know whether such a breakpoint
-     * exists for a given line number. (Else, we show a 'new' button).
-     * This indexes breakpoints by line number.
-     */
-    indexByLineNumber<T extends BreakpointMeta>(breakpoints: T[]): Map<number, T> {
-      const indexedBreakpoints = new Map<number, T>();
-      for (const bp of breakpoints) {
-        indexedBreakpoints.set(bp.location.line, bp);
+    /** Get the appropriate breakpoint market (new/active/completed) for each line. */
+    return rowNodes.map((node, index) => {
+      const lineNumber = index + 1;
+      // DOM node where marker will be portaled to.
+      const mountNode = this.getOrCreateBPMarkerMount(node, lineNumber);
+
+      if (activeBreakpoints.has(lineNumber)) {
+        return ReactDOM.createPortal(
+          <ActiveBreakpointMarker
+            breakpointMeta={activeBreakpoints.get(lineNumber)}
+          />,
+          mountNode
+        );
       }
-      return indexedBreakpoints;
-    }
 
-    render() {
-        const rowNodes = this.getRowNodes();
-        const activeBreakpoints = this.indexByLineNumber(this.props.activeBreakpoints);
-        const completedBreakpoints = this.indexByLineNumber(this.props.completedBreakpoints);
-
-        /** Get the appropriate breakpoint market (new/active/completed) for each line. */
-        return rowNodes.map((node, index) => {
-            const lineNumber = index + 1;
-            // DOM node where marker will be portaled to.
-            const mountNode = this.getOrCreateBPMarkerMount(node, lineNumber);
-
-            if (activeBreakpoints.has(lineNumber)) {
-                return ReactDOM.createPortal(<ActiveBreakpointMarker breakpointMeta={activeBreakpoints.get(lineNumber)}/>, mountNode);
-            }
-
-            if (completedBreakpoints.has(lineNumber)) {
-                return ReactDOM.createPortal(<CompletedBreakpointMarker breakpoint={completedBreakpoints.get(lineNumber)}/>, mountNode);
-            }
-            /** Note that this means we can't create new breakpoints on a line with existing breakpoints.
-             * TODO: address this somehow. May want to add something similar to cloud console with dropdown.
-             * In the short term, could just add ability to delete then re-create.
-             */
-            return ReactDOM.createPortal(<NewBreakpointMarker onClick={() => this.props.createBreakpoint(this.getFileName(), lineNumber)}/>, mountNode);
-        });
-
-    }
-
-    /** We are mounting breakpoint markers at the beginning of each line of code.
-     *  As github represents each line as a <tr/> this is a bit difficult, as React will only mount to the *end* of 
-     * a given container. Also, since it is a <tr/>, absolute positioning to move markers to the front won't work.
-     * 
-     * This logic injects a new mount point at the beginning of a given github code row. After that, it will return that
-     * mount point instead of re-creating it.
-     */
-    getOrCreateBPMarkerMount(rowNode, lineNumber):Node {
-        // Only create a new mount if it doesn't already exist.
-        if(!this.mountNodes.has(lineNumber)) {
-          const mountNode = document.createElement("div");
-          this.mountNodes.set(lineNumber, mountNode);
-          rowNode.prepend(mountNode);
-        }
-        return this.mountNodes.get(lineNumber);
+      if (completedBreakpoints.has(lineNumber)) {
+        return ReactDOM.createPortal(
+          <CompletedBreakpointMarker
+            breakpoint={completedBreakpoints.get(lineNumber)}
+          />,
+          mountNode
+        );
       }
+      /** Note that this means we can't create new breakpoints on a line with existing breakpoints.
+       * TODO: address this somehow. May want to add something similar to cloud console with dropdown.
+       * In the short term, could just add ability to delete then re-create.
+       */
+      return ReactDOM.createPortal(
+        <NewBreakpointMarker
+          onClick={() =>
+            this.props.createBreakpoint(this.getFileName(), lineNumber)
+          }
+        />,
+        mountNode
+      );
+    });
+  }
+
+  /** We are mounting breakpoint markers at the beginning of each line of code.
+   *  As github represents each line as a <tr/> this is a bit difficult, as React will only mount to the *end* of
+   * a given container. Also, since it is a <tr/>, absolute positioning to move markers to the front won't work.
+   *
+   * This logic injects a new mount point at the beginning of a given github code row. After that, it will return that
+   * mount point instead of re-creating it.
+   */
+  getOrCreateBPMarkerMount(rowNode, lineNumber): Node {
+    // Only create a new mount if it doesn't already exist.
+    if (!this.mountNodes.has(lineNumber)) {
+      const mountNode = document.createElement("div");
+      this.mountNodes.set(lineNumber, mountNode);
+      rowNode.prepend(mountNode);
+    }
+    return this.mountNodes.get(lineNumber);
+  }
 }
