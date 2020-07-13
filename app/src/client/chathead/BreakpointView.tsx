ddbf8d644d802ae4d26cd735f5b4913e2cb4e715
Jay Mohile <jamohile@google.com>
2020-07-13 15:35:19 -0400

    refactor: add material ui to breakpoint results

--- app/src/client/chathead/BreakpointView.tsx
+++ app/src/client/chathead/BreakpointView.tsx
@@ -1,28 +1,46 @@
 import React from "react";
+import { Accordion, AccordionSummary, Typography, List, ListItem, ListItemText, AccordionDetails, CircularProgress } from "@material-ui/core";
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 /** Used to display a breakpoint that has not yet hit. */
 export const PendingBreakpointView = ({ breakpointMeta }) => (
-  <div>
+  <Accordion>
     <BreakpointHeader
       id={breakpointMeta.id}
       location={breakpointMeta.location}
     />
-    <div>Loading...</div>
-  </div>
+    <AccordionDetails>
+      <CircularProgress/>
+    </AccordionDetails>
+  </Accordion>
 );
 
 /** Used to display data for a breakpoint that has already hit. */
-export const CompletedBreakpointView = ({ breakpoint }) => (
-  <div>
-    <BreakpointHeader id={breakpoint.id} location={breakpoint.location} />
-    {/* Adding the "null, 2" params enables pretty-printing*/}
-    <pre>{JSON.stringify(breakpoint, null, 2)}</pre>
-  </div>
-);
+export const CompletedBreakpointView = ({ breakpoint }) => {
+  //console.log(breakpoint);
+  const stackframe = breakpoint.stackFrames[0];
+  return (
+    <Accordion>
+      <BreakpointHeader id={breakpoint.id} location={breakpoint.location} />
+      <AccordionDetails>
+        <List>
+          {
+            stackframe.locals.map(variable => (
+              <ListItem>
+                <ListItemText primary={variable.name} secondary={variable.value}/>
+              </ListItem>
+            ))
+          }
+        </List>
+      </AccordionDetails>
+    </Accordion>
+  )
+};
 
 const BreakpointHeader = ({ id, location }) => (
-  <header>
-    <span>{location.path}</span>
-    <span>{location.line}</span>
-  </header>
+  <AccordionSummary expandIcon={<ExpandMoreIcon/>}>
+    <Typography>{location.path}</Typography>
+    {/* <span>{location.path}</span>
+    <span>{location.line}</span> */}
+  </AccordionSummary>
 );
