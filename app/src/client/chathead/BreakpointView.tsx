35a9bc6b74a0a981499e50715f4870a55b9a8914
Jay Mohile <jamohile@google.com>
2020-07-15 16:17:22 -0400

    feat: add separate views for failed and successful breakpoint

--- app/src/client/chathead/BreakpointView.tsx
+++ app/src/client/chathead/BreakpointView.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Accordion, AccordionSummary, Typography, List, ListItem, ListItemText, AccordionDetails, CircularProgress } from "@material-ui/core";
+import { Alert, AlertTitle } from "@material-ui/lab";
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import ErrorIcon from '@material-ui/icons/Error';
 import { Variable } from "../../common/types/debugger";
 
 /** Used to display a breakpoint that has not yet hit. */
@@ -17,6 +19,15 @@ export const PendingBreakpointView = ({ breakpointMeta }) => {
 
 /** Used to display data for a breakpoint that has already hit. */
 export const CompletedBreakpointView = ({ breakpoint }) => {
+  const {status} = breakpoint;
+  if (status.isError) {
+    return <SuccessfulCompletedBreakpointView breakpoint={breakpoint}/>;
+  }
+  return <FailedCompletedBreakpointView breakpoint={breakpoint}/>;
+};
+
+/** Shows stackframe data for a successful breakpoint. */
+export const SuccessfulCompletedBreakpointView = ({breakpoint}) => {
   const {stackFrames, location} = breakpoint;
   const stackframe = stackFrames[0];
   return (
@@ -28,8 +39,31 @@ export const CompletedBreakpointView = ({ breakpoint }) => {
         <VariablesView variables={stackframe.locals}/>
       </AccordionDetails>
     </Accordion>
-  )
-};
+  );
+}
+
+/** Shows error data for a failed breakpoint. */
+export const FailedCompletedBreakpointView = ({breakpoint}) => {
+  const {location, status} = breakpoint;
+  // Debugger returns the error message in the form of a bash style template string.
+  // Example: format = "Hello $0", parameters = ["Bob"]
+  const {format, parameters} = status.description;
+  // This Regex looks for sequences like $0, $1, ... and replaces them with the parameter for their index.
+  const message = format.replace(/\$({\d}+)/, (match, index) => parameters[index]);
+  return (
+    <Accordion defaultExpanded>
+      <AccordionSummary expandIcon={<ErrorIcon/>}>
+        <Typography>{location.path}:{location.line}</Typography>
+      </AccordionSummary>
+      <AccordionDetails>
+        <Alert severity="error">
+          <AlertTitle>{status.refersTo}</AlertTitle>
+          {message}
+        </Alert>
+      </AccordionDetails>
+    </Accordion>
+  );
+}
 
 /** Displays a set of variables from a debugger stack frame. */
 const VariablesView = ({variables}: {variables: Variable[]}) => {
