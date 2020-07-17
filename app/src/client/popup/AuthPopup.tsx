c6740e5bb7117accd182cc7b5a4abab94d33d6bf
hanane ikhelef <hananeikhelef@google.com>
2020-07-17 18:28:52 -0400

    feat: handle user login and off

--- app/src/client/popup/AuthPopup.tsx
+++ app/src/client/popup/AuthPopup.tsx
@@ -10,45 +10,66 @@ import {
 
 interface AuthPopupFormProps {}
 
-interface AuthPopupFormState {}
+interface AuthPopupFormState {
+  isAuthenticated: boolean;
+}
 export class AuthPopup extends React.Component<
   AuthPopupFormProps,
   AuthPopupFormState
 > {
   constructor(props: AuthPopupFormProps) {
     super(props);
+    this.state = {
+      isAuthenticated: false
+    }
+  }
+
+  async getAuthState() {
+    const response = await new GetAuthStateRequest().run(
+      new GetAuthStateRequestData()
+    );
+    this.setState({ isAuthenticated: response.isAuthenticated });
+    chrome.tabs.executeScript({
+      file: "injected-app.tsx",
+    });
   }
 
   render() {
-    const getAuthState = async () => {
-      const response = await new GetAuthStateRequest().run(
-        new GetAuthStateRequestData()
-      );
-      if (response.isAuthenticated) {
-        chrome.tabs.executeScript({
-          file: "Chathead.tsx",
-        });
-      }
-    };
-
-    return (
-      <>
-        <AppBar position="static">
-          <Toolbar>
-            <Typography variant="h6">Cloud Debugger</Typography>
-          </Toolbar>
-        </AppBar>
-        <Box style={{ paddingTop: "30px", paddingLeft: "30px", paddingBottom: "30px" }}>
-          <GoogleButton
-            onClick={async () => {
-              const response = await new AuthenticationRequest().run(
-                new AuthenticationRequestData()
-              );
-              return response;
+    console.log("state1 is :", this.state.isAuthenticated);
+    if (this.state.isAuthenticated == false) {
+      return (
+        <>
+          <AppBar position="static">
+            <Toolbar>
+              <Typography variant="h6">Cloud Debugger</Typography>
+            </Toolbar>
+          </AppBar>
+          <Box
+            style={{
+              paddingTop: "30px",
+              paddingLeft: "30px",
+              paddingBottom: "30px",
             }}
-          />
-        </Box>
-      </>
-    );
+          >
+            <GoogleButton
+              onClick={async () => {
+                await new AuthenticationRequest().run(
+                  new AuthenticationRequestData()
+                );
+                this.getAuthState();
+                console.log("state3 is :", this.state.isAuthenticated);
+              }}
+            />
+          </Box>
+        </>
+      );
+    }
+    else{
+      console.log("state2 is :", this.state.isAuthenticated);
+      chrome.tabs.executeScript({
+        file: "injected-app.tsx",
+      });
+      return;
+    }
   }
 }
