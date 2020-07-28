b9e85a8c3b8971ddf5f669362ab80cb307d1665c
KushalD1999 <kdalal@google.com>
2020-07-28 11:36:37 -0400

    Add testcase for enabling the services

--- api/__tests__/test-core-api.js
+++ api/__tests__/test-core-api.js
@@ -330,4 +330,32 @@ describe("Testing API functions", () => {
     expect(breakpoint).toEqual(mockResponse.response);
   });
 
+  it("Can enable given services in the project", async () => {
+    const mockProjectName= "projects/1234567/services/fooBar.googleapis.com";
+
+    const mockResponse = {
+      status: 200,
+      response: [{ service: "ENABLED" }],
+    };
+
+    // This depends on the number of "expect" checks we have below
+    expect.assertions(2);
+
+    // Before calling the API, set up a function to intercept the API request.
+    moxios.wait(() => {
+      const request = moxios.requests.mostRecent();
+
+      expect(request.config.url).toEqual(
+        "https://serviceusage.googleapis.com/v1/".concat(mockProjectName, ":enable")
+      );
+      request.respondWith(mockResponse);
+    });
+
+    // Call the API
+    const breakpoint = await api.enableService(
+      mockProjectName
+    );
+    expect(breakpoint).toEqual(mockResponse.response);
+  });
+
 });
