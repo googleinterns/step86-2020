f34fb5dc1607e2591df9373b27506b811f13a31e
hanane ikhelef <hananeikhelef@google.com>
2020-08-04 14:34:35 -0400

    add names

--- app/__tests__/chathead/Tutorial.tsx
+++ app/__tests__/chathead/Tutorial.tsx
@@ -1,15 +1,12 @@
 import React from "react";
-import Adapter from "enzyme-adapter-react-16";
 import { TutorialIntro } from "../../src/client/chathead/Tutorialintro";
 import { Step1Tutorial } from "../../src/client/chathead/Step1Tutorial";
 import ShallowRenderer from "react-test-renderer/shallow";
 import { Step2Tutorial } from "../../src/client/chathead/Step2Tutorial";
-import {ScrollDialogClass} from "../../src/client/chathead/Tutorial";
-import { shallow } from "enzyme";
 
 
 describe("Tutorial", () => {
-  it("tests first content of tutorial", () => {
+  it("tests intro content of tutorial", () => {
     const renderer = new ShallowRenderer();
     renderer.render(<TutorialIntro />);
     const result = renderer.getRenderOutput();
@@ -17,7 +14,7 @@ describe("Tutorial", () => {
     expect(result.type).toBe("section");
   });
 
-  it("tests first content of tutorial", () => {
+  it("tests first step content of tutorial", () => {
     const renderer = new ShallowRenderer();
     renderer.render(<Step1Tutorial />);
     const result = renderer.getRenderOutput();
@@ -25,20 +22,11 @@ describe("Tutorial", () => {
     expect(result.type).toBe("section");
   });
 
-  it("tests first content of tutorial", () => {
+  it("tests second content of tutorial", () => {
     const renderer = new ShallowRenderer();
     renderer.render(<Step2Tutorial />);
     const result = renderer.getRenderOutput();
 
     expect(result.type).toBe("section");
   });
-
-  // it("",() => {
-  //   const renderer = new ShallowRenderer();
-  //   renderer.render(<ScrollDialogClass />);
-  //   const result = renderer.getRenderOutput();
-
-  //   expect(result).toBe("section");
-  // });
-
 });
